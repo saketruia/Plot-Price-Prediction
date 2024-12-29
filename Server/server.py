f830c2c0137d95d80d56fff88bc9917829c08602
Saket <saketruia18@gmail.com>
2024-12-29 19:52:43 +0530

    UI completed

--- Server/server.py
+++ Server/server.py
@@ -1,15 +1,16 @@
-from flask import Flask, request , jsonify
+from flask import Flask, request, jsonify
 import util
 
-app =Flask(__name__)
+app = Flask(__name__)
 
-@app.route('/get_location_names')
+@app.route('/get_location_names', methods=['GET'])
 def get_location_names():
-    respsonse =jsonify({
-        'location': util.get_location_names()
+    response = jsonify({
+        'locations': util.get_location_names()
     })
-    response.headers.add('Access-Control-Allow-Origin','*')
-    return respsonse    
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
 
 @app.route('/predict_home_price', methods=['GET', 'POST'])
 def predict_home_price():
@@ -22,9 +23,10 @@ def predict_home_price():
         'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
+
     return response
 
-
-if __name__ =="__main__":
-    print("Starting Python Flask Server For Home Price Prediction")
+if __name__ == "__main__":
+    print("Starting Python Flask Server For Home Price Prediction...")
+    util.load_saved_artifacts()
     app.run()
